fed4314483d2ce951323d8abea7a168a65e7d1c4
p1petto <ksyuha3803@mail.ru>
2023-11-08 13:02:48 +0800

    added Notes and edited color picker

--- Scripts/AnimatedSprite2D.gd
+++ Scripts/AnimatedSprite2D.gd
@@ -8,19 +8,11 @@ func _on_color_picker_button_color_changed(color):
 
 func _on_color_picker_button_2_color_changed(color):
 	material.set("shader_parameter/newcolor2",color)
-	pass # Replace with function body.
+	pass
 
 
 func _on_color_picker_button_3_color_changed(color):
 	material.set("shader_parameter/newcolor3",color)
-	pass # Replace with function body.
+	pass 
 	
-func _ready():
-	var blue_value = 1
-	material.set_shader_parameter("blue", blue_value)
-	#material.set_shader_parameter("OLDCOLOR1",Color(0.78, 0.78, 0.78));
-	#print("ready")
-	#print(material.get_shader_parameter("OLDCOLOR1"))
-	#material.set_shader_parameter("NEWCOLOR1", Color(1, 1, 1, 1));
-	pass
 
