ed3573c870c9057f85e8b0acef22ed21a11da722
p1petto <ksyuha3803@mail.ru>
2024-10-22 23:33:14 +0800

    field: пикселизация линии

--- Scripts/drawing/ShowPixels.gd
+++ Scripts/drawing/ShowPixels.gd
@@ -8,12 +8,8 @@ func _ready() -> void:
 	pixels.array_received.connect(show_pixels)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func show_pixels():
-	print(pixels.color_array)
 	var img_width = 64
 	var img_height = 64
 	var img = Image.create(img_width, img_height, false, Image.FORMAT_RGBA8)
@@ -22,5 +18,4 @@ func show_pixels():
 		for x in range(pixels.color_array[y].size()):
 			img.set_pixel(x, y, Color(pixels.color_array[y][x]))  
 				
-	
 	texture = ImageTexture.create_from_image(img)
