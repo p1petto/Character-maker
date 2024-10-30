1dbe42923a8fd17dad5bb0182fda26b2a437179d
p1petto <ksyuha3803@mail.ru>
2024-10-30 15:26:22 +0800

    Изменяемое тело

---
+++ Scripts/drawing/body_part_border.gd
@@ -0,0 +1,13 @@
+extends Line2D
+
+
+@onready var body_part =  $"../.."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	points = body_part.array_points 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
